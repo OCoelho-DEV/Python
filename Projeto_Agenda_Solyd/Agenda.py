37d42e98c009aa0bd29a16ef1da5e3e6120bce0b
OCoelho-dev <coelhorafael512@gmail.com>
2026-05-23 22:45:59 -0300

    Implementação de incluir contato

--- Projeto_Agenda_Solyd/Agenda.py
+++ Projeto_Agenda_Solyd/Agenda.py
@@ -12,6 +12,15 @@ SCHEDULE = {
     }
 }
 
+def validate_string(func):
+    def wrapper(*args):
+        for value in args:
+            if not isinstance(value, str):
+                print(f'{value} must be a string')
+                return
+        return func(*args)
+    return wrapper
+
 def show_contacts(contact_name=None):
     if contact_name:
         contacts = {contact_name: SCHEDULE[contact_name]}
@@ -33,5 +42,16 @@ def search_contact(*args):
             print(f'Contact "{contact}" not found.')
             print('-' * 50)
 
+@validate_string
+def add_contact(name, number, email, address):
+    SCHEDULE[name] = {
+    "number": number,
+    "email": email,
+    "address": address
+}
+
+    
 # show_contacts()
-search_contact("João", "Giovana", "Rafael", "Maria")
+# search_contact("João", "Giovana", "Rafael", "Maria")
+add_contact("João", "(21) 99999-9999", "email1", "avenue 1")
+show_contacts()
