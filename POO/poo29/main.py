2c9d588c9d68c1f797462695052e6cbbc29fa2ce
OCoelho-dev <coelhorafael512@gmail.com>
2026-05-26 21:29:43 -0300

    Estudos dia 26/05/26

--- POO/poo29/main.py
+++ POO/poo29/main.py
@@ -1,7 +1,7 @@
-import uma_linha, varias_linhas
+import uma_linha, varias_linhas, documentando_funcoes
 
 # print(dir(uma_linha.__doc__))
 # print(uma_linha.__file__)
 # print(uma_linha.__name__)
 #help(uma_linha)
-help(varias_linhas)
+help(documentando_funcoes)
---
+++ POO/poo30.py
@@ -0,0 +1,31 @@
+# Enum -> Enumerações
+# Enumerações na programação, são usadas em ocasiões onde temos
+# um determinado número de coisas para escolher.
+# Enums têm membros e seus valores são constantes.
+# Enums em python:
+#   - são um conjunto de nomes simbólicos (membros) ligados a valores únicos
+#   - podem ser iterados para retornar seus membros canônicos na ordem de
+#       definição
+# enum.Enum é a superclasse para suas enumerações. Mas também pode ser usada
+#   diretamente (mesmo assim, Enums não são classes normais em Python).
+# Você poderá usar seu Enum com type annotations, com isinstance e
+# outras coisas relacionadas com tipo.
+# Para obter os dados:
+# membro = Classe(valor), Classe['chave']
+# chave = Classe.chave.name
+# valor = Classe.chave.value
+from enum import Enum
+
+class Directions(Enum):
+    left = 1
+    right = 2
+
+
+def move(direction: Directions):
+    if not isinstance(direction, Directions):
+        raise ValueError('Invalid move')
+    print(f'Moving to {direction.name} ({direction.value})')
+
+move(Directions.right)
+move(Directions.left)
+
