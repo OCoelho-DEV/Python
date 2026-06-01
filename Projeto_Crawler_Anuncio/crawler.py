d18bde42f711d8c68354800304e9468db3b9a147
OCoelho-dev <coelhorafael512@gmail.com>
2026-06-01 13:37:04 -0300

    Projeto avançando

--- Projeto_Crawler_Anuncio/crawler.py
+++ Projeto_Crawler_Anuncio/crawler.py
@@ -1,2 +1,28 @@
 import requests
+from bs4 import BeautifulSoup
 
+
+URL_AUTOMOBILES = "https://django-anuncios.solyd.com.br/automoveis/"
+
+def search(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            print(f'Not found: {response.status_code}')
+    except Exception as error:
+        print('Error at request')
+        print(error)
+
+def parsing(response_html):
+    try:
+        soup = BeautifulSoup(response_html, 'html.parser')
+        return soup
+    except Exception as error:
+        print('Error at parsing html:', error)
+
+response = search(URL_AUTOMOBILES)
+
+if response:
+    soup = parsing(response)
