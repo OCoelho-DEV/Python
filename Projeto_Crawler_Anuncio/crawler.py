ec8544e72bb350b77ee4830c112d77b49f9d0ed6
OCoelho-dev <coelhorafael512@gmail.com>
2026-06-03 18:05:14 -0300

    Regex para encontrar números

--- Projeto_Crawler_Anuncio/crawler.py
+++ Projeto_Crawler_Anuncio/crawler.py
@@ -1,10 +1,12 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
 DOMAIN = 'https://django-anuncios.solyd.com.br'
 URL_AUTOMOBILES = "https://django-anuncios.solyd.com.br/automoveis/"
 
-def search(url):
+def request(url):
     try:
         response = requests.get(url)
         if response.status_code == 200:
@@ -23,21 +25,47 @@ def parsing(response_html):
         print('Error at parsing html:', error)
 
 def find_links(soup):
-    cards_parent = soup.find('div', class_='ui three doubling link cards')
-    cards = cards_parent.find_all('a')
+    try:
+        cards_parent = soup.find('div', class_='ui three doubling link cards')
+        cards = cards_parent.find_all('a')
+    except:
+        print('Error at find cards on link')
 
     links = []
     for card in cards:
-        link = card['href']
-        links.append(link)
-
+        try:
+            link = card['href']
+            links.append(link)
+        except:
+            pass
     return links
 
-response = search(URL_AUTOMOBILES)
-
-if response:
-    soup = parsing(response)
-    if soup:
-        links = find_links(soup)
-        print(links)
+def find_number(soup):
+    try:
+        description = soup.find_all('div', class_='sixteen wide column')[2].p.\
+            get_text().strip()
+    except:
+        print('Error as find href description')
+        return
     
+    regex = re.findall(
+        r'\(?0?([1-9]{2})[ \-\.\)]{0,2}(9?[ \-\.]?\d{4})[ \-\.]?(\d{4})',
+        description)
+    
+    if regex:
+        return regex
+
+response_search = request(URL_AUTOMOBILES)
+
+if response_search:
+    soup_search = parsing(response_search)
+    if soup_search:
+        links = find_links(soup_search)
+        for link in links:
+            response_announcement = request(DOMAIN + link)
+            if response_announcement:
+                soup_announcement = parsing(response_announcement)
+                if soup_announcement:
+                    number = find_number(soup_announcement)
+                    print(number)
+
